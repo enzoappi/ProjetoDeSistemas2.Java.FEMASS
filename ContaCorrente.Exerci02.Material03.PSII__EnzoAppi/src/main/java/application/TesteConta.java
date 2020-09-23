181925b08bec5573a87d060c5b6a4af382fb46cd
Enzo Tiriba Appi <enzoappi.tiriba@gmail.com>
2020-09-23 13:45:09 -0300

    Adicao do Tratamento de excecao na classe principal!

--- ContaCorrente.Exerci02.Material03.PSII__EnzoAppi/src/main/java/application/TesteConta.java
+++ ContaCorrente.Exerci02.Material03.PSII__EnzoAppi/src/main/java/application/TesteConta.java
@@ -5,6 +5,7 @@
  */
 package application;
 
+import java.util.InputMismatchException;
 import javax.swing.JOptionPane;
 import model.Conta;
 
@@ -14,36 +15,43 @@ import model.Conta;
  */
 public class TesteConta {
     public static void main(String[] args) {
-        Conta conta = new Conta(500.0);
-        
-        System.out.println("\n\nVamos criando uma conta nova...");
-        System.out.println("Saldo original da conta: " + conta.getSaldo());
-        System.out.println("Status da conta: " + conta.getEstadoConta());
-        
-        System.out.println("\n\nVamos sacar dessa conta...");
-        conta.saque(600.0);
-        System.out.println("Saldo apos saque: " + conta.getSaldo());
-        System.out.println("Status da conta: " + conta.getEstadoConta());
-        
-        System.out.println("\n\nVamos sacar dessa conta, mais uma vez...");
-        conta.saque(100.0);
-        System.out.println("Saldo apos saque: " + conta.getSaldo());
-        System.out.println("Status da conta: " + conta.getEstadoConta());
-        
-        System.out.println("\n\nVamos depositar nessa conta...");
-        conta.deposito(100.0);
-        System.out.println("Saldo apos o depósito: " + conta.getSaldo());
-        System.out.println("Status da conta: " + conta.getEstadoConta());
-        
-        System.out.println("\n\nVamos continuar a depositar nessa conta...");
-        conta.deposito(600.0);
-        System.out.println("Saldo apos o depósito: " + conta.getSaldo());
-        System.out.println("Status da conta: " + conta.getEstadoConta());
-        
-        System.out.println("\n\nVamos sacar dessa conta, mais uma vez...");
-        conta.saque(1500.0);
-        System.out.println("Saldo apos o depósito: " + conta.getSaldo());
-        System.out.println("Status da conta: " + conta.getEstadoConta());
-        
+        try{
+            Conta conta = new Conta(500.0);
+            
+            System.out.println("\n\nCriando uma conta nova...");
+            System.out.println("Saldo original da conta: " + conta.getSaldo());
+            System.out.println("Status da conta: " + conta.getEstadoConta());
+
+            System.out.println("\n\nVamos sacar dessa conta...");
+            conta.saque(600.0);
+            System.out.println("Saldo apos saque: " + conta.getSaldo());
+            System.out.println("Status da conta: " + conta.getEstadoConta());
+
+            System.out.println("\n\nVamos sacar dessa conta, mais uma vez...");
+            conta.saque(100.0);
+            System.out.println("Saldo apos saque: " + conta.getSaldo());
+            System.out.println("Status da conta: " + conta.getEstadoConta());
+
+            System.out.println("\n\nVamos depositar nessa conta...");
+            conta.deposito(100.0);
+            System.out.println("Saldo apos o depósito: " + conta.getSaldo());
+            System.out.println("Status da conta: " + conta.getEstadoConta());
+
+            System.out.println("\n\nVamos continuar a depositar nessa conta...");
+            conta.deposito(600.0);
+            System.out.println("Saldo apos o depósito: " + conta.getSaldo());
+            System.out.println("Status da conta: " + conta.getEstadoConta());
+
+            System.out.println("\n\nVamos sacar dessa conta, mais uma vez...");
+            conta.saque(1500.0);
+            System.out.println("Saldo apos o depósito: " + conta.getSaldo());
+            System.out.println("Status da conta: " + conta.getEstadoConta());
+            System.out.println("\n\n");
+            
+        }
+        catch(InputMismatchException ime){
+            System.out.println("\n\nErro: " + ime.getMessage());
+            System.out.println("\n\n");
+        }
     }
 }
